679dd5d623e4dc9517dfb23ff2b7f3def3a18668
GabrielPaixaoo <72279737+GabrielPaixaoo@users.noreply.github.com>
2020-10-02 22:13:09 +0000

    Update App.java

--- lab1/src/edu/poo/app/App.java
+++ lab1/src/edu/poo/app/App.java
@@ -19,5 +19,6 @@ public class App
         System.out.println("3. Gustavo Silveira");
         System.out.println("4. Kevin Kasantaroeno");//@kevinkasantaroeno - Membro
         System.out.println("5. Igor Ximenes");
+        Sustém.out.println("6. Gabriel Paixão");// membro
     }
 }
