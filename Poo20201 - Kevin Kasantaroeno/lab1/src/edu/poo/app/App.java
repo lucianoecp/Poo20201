fcb815de24b7794cf3dae35a1ab5b2b112471711
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-10-03 13:26:03 -0300

    Update App.java
    Adicionei meu GitHub em um comentário

--- Poo20201 - Kevin Kasantaroeno/lab1/src/edu/poo/app/App.java
+++ Poo20201 - Kevin Kasantaroeno/lab1/src/edu/poo/app/App.java
@@ -3,6 +3,6 @@ package edu.poo.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Equipe 5: \n");
-        System.out.println("Kevin Kasantaroeno - Membro");
+        System.out.println("Kevin Kasantaroeno - Membro");//@kevinkasantaroeno - GitHub
     }
 }
