7fd86e1324c3cbe44e9f6b9095ef1638d995c0ab
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-10-03 13:24:24 -0300

    Add files via upload

---
+++ Poo20201 - Kevin Kasantaroeno/lab1/src/edu/poo/app/App.java
@@ -0,0 +1,8 @@
+package edu.poo.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Equipe 5: \n");
+        System.out.println("Kevin Kasantaroeno - Membro");
+    }
+}
