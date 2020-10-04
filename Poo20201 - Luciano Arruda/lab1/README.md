ba3e24886cbea640ab9519ee66904a441430dca9
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2020-10-04 15:51:29 -0300

    upload do laboratório individual

---
+++ Poo20201 - Luciano Arruda/lab1/README.md
@@ -0,0 +1,11 @@
+## Programação Orientada a Objetos
+### Laboratório 1 - Criando uma aplicação
+
+##### aluno: Luciano Arruda
+
+
+
+
+
+
+
---
+++ Poo20201 - Luciano Arruda/lab1/src/edu/poo/app/App.java
@@ -0,0 +1,20 @@
+package edu.poo.app;
+
+/**
+ * Isso é um comentário para documentação
+ * @author Luciano Arruda
+ */
+
+public class App 
+{
+    /*
+    Isso é um comentário de múltipla linha  
+    */
+
+    public static void main(String[] args)
+    {
+        // Isso é um comentário de linha única
+        System.out.println((char)34+"Somos autómatos em três quartas partes das nossas ações."
+        +(char)34+"\n\t\t\t\t\t(Wilhelm Leibniz)");
+   }
+}
