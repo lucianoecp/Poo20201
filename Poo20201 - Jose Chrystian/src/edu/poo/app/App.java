14ff80f545a93d08936bf4b1f5134996b4aec780
ChrystianGreen2 <71555983+ChrystianGreen2@users.noreply.github.com>
2020-10-05 16:29:14 -0300

    .

---
+++ Poo20201 - Jose Chrystian/src/edu/poo/app/App.java
@@ -0,0 +1,19 @@
+package edu.poo.app;
+
+/**
+ * Comentário para documentação
+ */
+
+
+public class App 
+{
+    /*
+    Comentário de múltipla linha
+    */
+    public static void main(String[] args)
+    {
+        System.out.println("Equipe 5:\n");
+        //integrantes da equipe 
+        System.out.println("2. José Chrystian -  Membro");
+    }
+}
