cebecc572697b8331995fa8fc3a148bbe74957f8
ChrystianGreen2 <71555983+ChrystianGreen2@users.noreply.github.com>
2020-10-08 19:59:26 -0300

    Update

---
+++ Poo20201 - Jose Chrystian/Labs/src/edu/poo/Lab2/Variaveis1.java
@@ -0,0 +1,15 @@
+public class Variaveis1 {
+	
+	public static void main(String[] args) {
+		int value;
+		value = 10000;
+
+		int data = 10000;
+
+		int totalValue;
+		int notas;
+		int saldoConta;
+		int treinamento;
+	}
+
+}
---
+++ Poo20201 - Jose Chrystian/Labs/src/edu/poo/Lab2/Variaveis2.java
@@ -0,0 +1,21 @@
+public class Variaveis2 {
+    
+    public static void main(String[] args) {
+        int varTeste;
+        System.out.println(varTeste);
+        
+        varTeste = 50;
+        System.out.println(varTeste);
+        
+        int varTeste2 = 100;
+        System.out.println(varTeste2);
+        
+        varTeste = varTeste2;
+        System.out.println(varTeste);
+        
+        varTeste2 = 200;
+        System.out.println(varTeste);
+        
+        System.out.println(varTeste2);
+    }
+}
