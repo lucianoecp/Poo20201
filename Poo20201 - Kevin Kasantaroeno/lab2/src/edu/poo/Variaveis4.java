2619d70c1ced60d53ba6d5d9836f12c23f99f41b
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-10-09 22:06:42 -0300

    Update Variaveis4.java

--- Poo20201 - Kevin Kasantaroeno/lab2/src/edu/poo/Variaveis4.java
+++ Poo20201 - Kevin Kasantaroeno/lab2/src/edu/poo/Variaveis4.java
@@ -2,9 +2,9 @@ package edu.poo;
 
 public class Variaveis4 {
     public static void main(String[] args){
-        int primeiroValor = 100;
+        int primeiroValor = 400;
         int outroValor = primeiroValor;
-        primeiroValor = 50;
+        primeiroValor = 200;
         System.out.println(String.format("%s, %s", primeiroValor, outroValor));
     }
 }
