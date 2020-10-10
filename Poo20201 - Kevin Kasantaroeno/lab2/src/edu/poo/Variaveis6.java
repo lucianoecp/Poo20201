2ac5fd58e447f02bf23b11f2a12b4a2f1a114881
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-10-09 22:08:09 -0300

    Update Variaveis6.java

--- Poo20201 - Kevin Kasantaroeno/lab2/src/edu/poo/Variaveis6.java
+++ Poo20201 - Kevin Kasantaroeno/lab2/src/edu/poo/Variaveis6.java
@@ -2,14 +2,14 @@ package edu.poo;
 
 public class Variaveis6 {
     public static void main(String[] args){
-        int valor = 50;
-        valor -= 5;
+        int valor =80;
+        valor -= 10;
         System.out.println(valor);
 
-        int resultado = 100;
+        int resultado = 200;
         System.out.println(resultado);
 
-        int valor1 = 5;
+        int valor1 = 10;
         int valor2 = 10;
         resultado /= valor1 * valor2;
         System.out.println(resultado);
