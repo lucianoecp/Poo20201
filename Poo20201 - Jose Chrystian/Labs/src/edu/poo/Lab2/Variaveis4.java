2a697b01e6801079a97a48ffbe7feab22d221c86
ChrystianGreen2 <71555983+ChrystianGreen2@users.noreply.github.com>
2020-10-09 23:33:25 -0300

    update

--- Poo20201 - Jose Chrystian/Labs/src/edu/poo/Lab2/Variaveis4.java
+++ Poo20201 - Jose Chrystian/Labs/src/edu/poo/Lab2/Variaveis4.java
@@ -1,5 +1,9 @@
-public class Variaveis5 {
-          public static void main(String[] args) {
-            System.out.println('empty');
-        }
-    }
+public class Variaveis4 {
+  public static void main(String[] args) {
+    // tipos primitivos sao armazenados por valor
+    int firstValue= 100;
+    int secondValue= firstValue;
+    firstValue= 50;
+    System.out.println(String.format("%s, %s", firstValue, secondValue));
+}
+}
--- Poo20201 - Jose Chrystian/Labs/src/edu/poo/Lab2/Variaveis5.java
+++ Poo20201 - Jose Chrystian/Labs/src/edu/poo/Lab2/Variaveis5.java
@@ -1,9 +1,14 @@
 public class Variaveis5 {
-    public static void main(String[] args) {
-      // tipos primitivos sao armazenados por valor
-      int firstValue= 100;
-      int secondValue= firstValue;
-      firstValue= 50;
-      System.out.println(String.format("%s, %s", firstValue, secondValue));
+  public static void main(String[] args) {
+  // operadores pre-fixados/pos-fixados
+    int valor = 200;
+    System.out.println(++valor);
+    System.out.println(++valor);
+    System.out.println(++valor);
+    System.out.println(++valor); 
+    System.out.println(valor); 
+    int valor2= 5;
+    System.out.println(valor2++); 
+    System.out.println(valor + valor2); 
   }
-}
+  }
