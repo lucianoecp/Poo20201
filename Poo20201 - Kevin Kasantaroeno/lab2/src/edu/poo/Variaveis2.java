342d620b4d5fc28154ed5bad641d2782b71d14ce
kevinkasantaroeno <71191665+kevinkasantaroeno@users.noreply.github.com>
2020-10-09 22:05:59 -0300

    Update Variaveis2.java

--- Poo20201 - Kevin Kasantaroeno/lab2/src/edu/poo/Variaveis2.java
+++ Poo20201 - Kevin Kasantaroeno/lab2/src/edu/poo/Variaveis2.java
@@ -5,16 +5,16 @@ public class Variaveis2 {
         int variavel = 0;
         System.out.println(variavel);
 
-        variavel = 50;
+        variavel = 350;
         System.out.println(variavel);
 
-        int outraVariavel = 100;
+        int outraVariavel = 500;
         System.out.println(outraVariavel);
 
         variavel = outraVariavel;
         System.out.println(variavel);
 
-        outraVariavel = 200;
+        outraVariavel = 600;
         System.out.println(outraVariavel);
 
         System.out.println(variavel);
