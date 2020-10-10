6d16498e177139beb836ddecf0e5e44a47441643
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2020-10-10 07:03:52 -0300

    Add Variaveis5.java

---
+++ Poo20201 - Luciano Arruda/lab2/src/poo/aula3/Variaveis5.java
@@ -0,0 +1,19 @@
+package poo.aula3;
+
+public class Variaveis5 {
+
+    public static void main(String[] args) 
+    {
+        // Operadores pré-fixados vs pós-fixados:
+        int valor = 6;
+        System.out.println(++valor); //7
+        System.out.println(valor); //700
+
+        valor = 6;
+        System.out.println(valor++); // 7
+        System.out.println(valor);
+
+        
+    }
+    
+}
