bf790237a8412ac81b87dbbe91a048b856efbdd6
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2020-10-10 05:36:42 -0300

    iniciando lab2

---
+++ Poo20201 - Luciano Arruda/lab2/src/poo/aula3/Variaveis1.java
@@ -0,0 +1,20 @@
+package poo.aula3;
+
+public class Variaveis1 
+{
+
+    public static void main(String[] args) 
+    {
+        // Declaração simples de variável:
+        String nome;
+        // Inicialização de variável:
+        nome = "Luciano Arruda";
+
+        // Declaração com inicialização:
+        int idade = 25;
+
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
